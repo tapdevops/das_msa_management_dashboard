0b6e81735b027c2059b234fecf99970cbe192554
Rino <Rino>
2020-05-14 10:12:54 +0700

    error dev

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
 module.exports = {
+	dev: {
+		user          : process.env.NODE_ORACLEDB_USER,
+		password      : process.env.NODE_ORACLEDB_PASSWORD,
+		connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING
+	},
 	prod: {
 		user          : process.env.NODE_ORACLEDB_USER,
 		password      : process.env.NODE_ORACLEDB_PASSWORD,
